a3fa5618b2ff9bb980d913406dfb692dce9dde52
sheilovani <sheilayovani@gmail.com>
2020-10-22 10:35:40 +0700

    update function.py

--- number_8/function.py
+++ number_8/function.py
@@ -1,12 +1,6 @@
-import collections
-
 def filter_duplicates(arr1, arr2):
     mergedArr = arr1 + arr2
     seen = set()
     seen_add = seen.add
     duplicates = set( x for x in mergedArr if x in seen or seen_add(x) )
     return list( duplicates )
-
-a = [1,2,3]
-b = [2,3,4,5]
-print(filter_duplicates(a, b))
